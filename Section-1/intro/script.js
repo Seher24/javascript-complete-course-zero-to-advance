b6131c071318b0f0e00a4629566aa4bd01cf7df7
SeherSiddique23 <seher.siddique@squatwolf.com>
2026-04-22 14:09:31 +0500

    Introduction to Javascript
    Section 1
    .Introduction to JavaScript
    .First JavaScript Program

---
+++ Section-1/intro/script.js
@@ -0,0 +1,4 @@
+alert("Hello, World!");
+document.write("Hello, World!");
+console.log("Hello, World!");
+// we can also execute the javascript code in the console of the browser. To open the console, press F12 and go to the console tab. Then you can type your javascript code and press enter to execute it.
