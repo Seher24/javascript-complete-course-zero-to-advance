890f30c0063e5cd50ee308837a52943e76df075e
SeherSiddique23 <seher.siddique@squatwolf.com>
2026-04-22 15:22:33 +0500

    added scope .

---
+++ Section-2/scope/script.js
@@ -0,0 +1,2 @@
+// scope
+// Scope is the area of the code where a variable is defined and can be accessed. There are three types of scope in JavaScript: global scope, function scope, and block scope.
