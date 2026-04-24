7fe44191f73b2623880c8df93668271029849f10
SeherSiddique23 <seher.siddique@squatwolf.com>
2026-04-24 20:06:08 +0500

    data-types added to section 2.
    data-types added to section 2.

---
+++ Section-2/data-types/primitive/script.js
@@ -0,0 +1,39 @@
+// Data Types in Javascript
+// Primitives and Non-Primitives
+
+// Primitives
+// String, Number, Boolean, Null, Undefined, Symbol
+
+// String Data Type
+let firstName = "Seher";
+let lastName = "Siddique";
+console.log(firstName + " " + lastName);
+let num = "10";
+console.log(typeof num);
+// Number Data Type
+let age = 25;
+let height = 5.0;
+console.log(age);
+console.log(height);
+
+// Boolean Data Type
+let isStudent = true;
+let isEmployed = false;
+console.log(typeof isStudent);
+console.log(isEmployed);
+let x = 20 > 10;
+console.log(x); //true
+
+// undefined Data Type
+let number1;
+console.log(number1); //undefined->value
+console.log(typeof number1); //undefined
+
+// null Data Type
+let number2 = null;
+console.log(number2);
+console.log(typeof number2); //object
+console.log(null == undefined); //true
+
+// Non-Primitives
+// Object, Array, Function
---
+++ Section-2/data-types/reference/script.js
@@ -0,0 +1,27 @@
+// reference data types
+// objects
+let person = {
+  name: "Seher",
+  age: 25,
+  city: "Karachi",
+};
+console.log(person);
+console.log(typeof person);
+
+// array
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+console.log(typeof numbers);
+
+// function
+function greet() {
+  console.log("Hello, World!");
+}
+greet();
+console.log(typeof greet);
+
+let a;
+console.log(a); //undefined
+console.log(typeof a); //undefined
+a = "Seher Siddique";
+console.log(a); //Seher Siddique
